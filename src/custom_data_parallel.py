e3a27a7f324af498aaada0edb058a8bd8f96c43a
Gabriela Melo <gabimelo10@gmail.com>
2019-05-30 14:49:58 -0300

    remove comment and old code in custom data parallel

--- src/custom_data_parallel.py
+++ src/custom_data_parallel.py
@@ -7,10 +7,9 @@ class CustomDataParallel(nn.Module):
     def __init__(self, model):
         super(CustomDataParallel, self).__init__()
         self.model = DataParallelModel(model)
-#         .cuda()
 
-    def forward(self, input, hidden):
-        return self.model(input, hidden)
+    def forward(self, *input):
+        return self.model(*input)
 
     def __getattr__(self, name):
         try:
