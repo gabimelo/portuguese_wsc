17ab3a649859a4d46666331c9940c8e6ea7e40c2
Gabriela Melo <gabimelo10@gmail.com>
2019-06-27 19:27:27 -0300

    fix test for get_latest_file

--- tests/test_helpers/test_utils/test_get_latest_file.py
+++ tests/test_helpers/test_utils/test_get_latest_file.py
@@ -6,4 +6,4 @@ from src.helpers.utils import get_latest_model_file
 class TestGetLatestFile(object):
     @mock.patch('src.helpers.utils.MODEL_FILE_NAME', 'tests/test_helpers/test_utils/mock_dir/{}.pt')
     def test_get_latest_file(self):
-        assert get_latest_model_file() == 'tests/test_helpers/test_utils/mock_dir/newer_file.pt'
+        assert get_latest_model_file() == 'tests/test_helpers/test_utils/mock_dir/newest_file.pt'
