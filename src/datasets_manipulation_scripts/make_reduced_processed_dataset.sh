b40f6ef930a0a04aeee4eabf2d527ab8a2c3bfa7
Gabriela Melo <gabimelo10@gmail.com>
2019-07-11 00:44:23 -0300

    make processed dataset scripts not overwrite one another

--- src/datasets_manipulation_scripts/make_reduced_processed_dataset.sh
+++ src/datasets_manipulation_scripts/make_reduced_processed_dataset.sh
@@ -2,15 +2,15 @@
 
 mkdir data/processed/splits
 
-cp data/processed/train.txt data/processed/train_bkp.txt
+cp data/processed/train.txt data/processed/train_full.txt
 split -C 11m --numeric-suffixes data/processed/train.txt data/processed/splits/train_
 cp data/processed/splits/train_00 data/processed/train.txt
 
-cp data/processed/train.txt data/processed/test_bkp.txt
+cp data/processed/train.txt data/processed/test_full.txt
 split -C 2m --numeric-suffixes data/processed/test.txt data/processed/splits/test_
 cp data/processed/splits/test_00 data/processed/test.txt
 
-cp data/processed/train.txt data/processed/valid_bkp.txt
+cp data/processed/train.txt data/processed/valid_full.txt
 split -C 2m --numeric-suffixes data/processed/valid.txt data/processed/splits/valid_
 cp data/processed/splits/valid_00 data/processed/valid.txt
 
