646324fb5c34edd424143eb98900314d94984e2a
Gabriela Melo <gabimelo10@gmail.com>
2019-07-27 16:13:27 -0300

    fix make reduced processed dataset script

--- src/datasets_manipulation_scripts/make_reduced_processed_dataset.sh
+++ src/datasets_manipulation_scripts/make_reduced_processed_dataset.sh
@@ -2,16 +2,16 @@
 
 mkdir data/processed/splits
 
-cp data/processed/train.txt data/processed/train_full.txt
 split -C 11m --numeric-suffixes data/processed/train.txt data/processed/splits/train_
+cp data/processed/train.txt data/processed/train_full.txt
 cp data/processed/splits/train_00 data/processed/train.txt
 
-cp data/processed/train.txt data/processed/test_full.txt
 split -C 2m --numeric-suffixes data/processed/test.txt data/processed/splits/test_
+cp data/processed/test.txt data/processed/test_full.txt
 cp data/processed/splits/test_00 data/processed/test.txt
 
-cp data/processed/train.txt data/processed/valid_full.txt
 split -C 2m --numeric-suffixes data/processed/valid.txt data/processed/splits/valid_
+cp data/processed/valid.txt data/processed/valid_full.txt
 cp data/processed/splits/valid_00 data/processed/valid.txt
 
 rm -rf data/processed/splits
