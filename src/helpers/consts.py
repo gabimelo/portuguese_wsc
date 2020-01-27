d6dbc4f39d8f5a9fd1f1dd23ba4e83f08766737d
Gabriela Melo <gabimelo10@gmail.com>
2020-01-26 23:35:37 -0200

    add comment about pt names file

--- src/helpers/consts.py
+++ src/helpers/consts.py
@@ -19,6 +19,7 @@ PORTUGUESE = True
 if PORTUGUESE:
     WINOGRAD_SCHEMAS_ORIGINAL_FILE = ''
     WINOGRAD_SCHEMAS_FILE = f'{PROCESSED_DATA_DIR_NAME}/portuguese_wsc.json'
+    # WINOGRAD_SCHEMAS_FILE = f'{PROCESSED_DATA_DIR_NAME}/portuguese_wsc_portuguese_names.json'
     FILTER_WORDS = 3
 else:
     WINOGRAD_SCHEMAS_ORIGINAL_FILE = f'{RAW_DATA_DIR_NAME}/english_wsc.json'
