fafd69b75311b73ca4886902391bf90a1f345440
Gabriela Melo <gabimelo10@gmail.com>
2020-01-27 19:23:56 -0300

    reduce train size to 30M

--- src/datasets_manipulation_scripts/make_processed_dataset.sh
+++ src/datasets_manipulation_scripts/make_processed_dataset.sh
@@ -2,10 +2,10 @@
 
 mkdir data/processed/splits
 
-split -C 40m --numeric-suffixes data/interim/wiki_pt_splits/wiki_pt00.txt data/processed/splits/main_
+split -C 30m --numeric-suffixes data/interim/wiki_pt_splits/wiki_pt00.txt data/processed/splits/main_
 # split -C 1200k --numeric-suffixes data/processed/splits/main_01 data/processed/splits/test_val_
 
-cp data/processed/train.txt data/processed/train_9M_tokens.txt
+cp data/processed/train.txt data/processed/train_7M_tokens.txt
 cp data/processed/splits/main_00 data/processed/train.txt
 # cp data/processed/splits/test_val_00 data/processed/test.txt
 # cp data/processed/splits/test_val_01 data/processed/valid.txt
