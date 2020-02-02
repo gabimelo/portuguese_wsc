00ec8626f8e8b37d873acce5167e6b2336fca0c0
Gabriela Melo <gabimelo10@gmail.com>
2020-02-01 22:37:57 -0300

    refactor get_sentence_breaks

--- tests/test_language_model_usage/test_winograd_schema_challenge/test_get_sentence_breaks.py
+++ tests/test_language_model_usage/test_winograd_schema_challenge/test_get_sentence_breaks.py
@@ -7,29 +7,22 @@ class TestGetSentenceBreaks:
             'The city councilmen refused the demonstrators a permit because the city councilmen feared violence.'
         second_sentence = \
             'The city councilmen refused the demonstrators a permit because the demonstrators feared violence.'
-        i = get_sentence_breaks(first_sentence, second_sentence)
-        assert ' '.join(first_sentence.split()[:i]) == \
-            'The city councilmen refused the demonstrators a permit because the'
-        assert ' '.join(second_sentence.split()[:i]) == \
-            'The city councilmen refused the demonstrators a permit because the'
-        assert ' '.join(first_sentence.split()[i:]) == \
-            'city councilmen feared violence.'
-        assert ' '.join(second_sentence.split()[i:]) == \
-            'demonstrators feared violence.'
-        assert ' '.join(first_sentence.split()[:i]) == ' '.join(second_sentence.split()[:i])
+        correct_sentence_first_part, correct_sentence_second_part, wrong_sentence_first_part, wrong_sentence_second_part = \
+            get_sentence_breaks(first_sentence, second_sentence)
+
+        assert correct_sentence_first_part == 'The city councilmen refused the demonstrators a permit because the'
+        assert correct_sentence_first_part == wrong_sentence_first_part
+        assert correct_sentence_second_part == 'city councilmen feared violence.'
+        assert wrong_sentence_second_part == 'demonstrators feared violence.'
 
         first_sentence = \
             'Os vereadores recusaram a autorização aos manifestantes porque os vereadores temiam a violência.'
         second_sentence = \
             'Os vereadores recusaram a autorização aos manifestantes porque os manifestantes temiam a violência.'
+        correct_sentence_first_part, correct_sentence_second_part, wrong_sentence_first_part, wrong_sentence_second_part = \
+            get_sentence_breaks(first_sentence, second_sentence)
 
-        i = get_sentence_breaks(first_sentence, second_sentence)
-        assert ' '.join(first_sentence.split()[:i]) == \
-            'Os vereadores recusaram a autorização aos manifestantes porque os'
-        assert ' '.join(second_sentence.split()[:i]) == \
-            'Os vereadores recusaram a autorização aos manifestantes porque os'
-        assert ' '.join(first_sentence.split()[i:]) == \
-            'vereadores temiam a violência.'
-        assert ' '.join(second_sentence.split()[i:]) == \
-            'manifestantes temiam a violência.'
-        assert ' '.join(first_sentence.split()[:i]) == ' '.join(second_sentence.split()[:i])
+        assert correct_sentence_first_part == 'Os vereadores recusaram a autorização aos manifestantes porque os'
+        assert correct_sentence_first_part == wrong_sentence_first_part
+        assert correct_sentence_second_part == 'vereadores temiam a violência.'
+        assert wrong_sentence_second_part == 'manifestantes temiam a violência.'
